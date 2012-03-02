228436d539798aefb9db913c677ceb54c2b7d7ab
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-03-02 15:32:59 -0500

    first

---
+++ README.md
@@ -0,0 +1,24 @@
+# aug3d - Augmented Reality with 3D glasses
+
+## EXAMPLES
+
+	$ open shot.png
+
+or
+
+	$ python aug3d.py
+	...
+
+## CREDITS
+
+### Libraries
+
+[SimpleCV](http://simplecv.org/)
+
+### Haar patterns
+
+[OpenCV](http://opencv.willowgarage.com/wiki/)
+
+### Shades
+
+[OpenClipArt](http://openclipart.org/)
---
+++ aug3d.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# aug3d
+# Andrew Pennebaker
+# 2 March 2012
+
+import SimpleCV
+import random
+import sys
+import os
+import time
+
+def main():
+    camera = SimpleCV.Camera()
+
+    d = os.path.abspath(os.path.dirname(sys.argv[0])) + os.sep
+
+    haar = d + "haar-eyes.xml"
+
+    shades = SimpleCV.ImageClass.Image(d + "glasses.png")
+
+    while True:
+        image = camera.getImage()
+
+        features = image.findHaarFeatures(haar)
+
+        try:
+            # features.draw(SimpleCV.Color.GREEN)
+
+            for i in range(len(features)):
+                feature = features[i]
+                coords = feature.coordinates()
+
+                image.blit(shades, (coords[0], coords[1]), True)
+        except:
+            pass
+
+        image.show()
+
+        time.sleep(0.05)
+
+if __name__=="__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
