f434a038791a1c83fbc56fb41120c5a258be5925
mcandre <andrew.pennebaker@gmail.com>
2013-04-30 10:43:04 -0400

    tabs

--- aug3d.py
+++ aug3d.py
@@ -2,7 +2,6 @@
 
 # aug3d
 # Andrew Pennebaker
-# 2 March 2012
 
 import SimpleCV
 import sys
@@ -10,36 +9,36 @@ import os
 import time
 
 def main():
-    camera = SimpleCV.Camera()
+  camera = SimpleCV.Camera()
 
-    d = os.path.abspath(os.path.dirname(sys.argv[0])) + os.sep
+  d = os.path.abspath(os.path.dirname(sys.argv[0])) + os.sep
 
-    haar = d + "haar-eyes.xml"
+  haar = d + "haar-eyes.xml"
 
-    shades = SimpleCV.ImageClass.Image(d + "glasses.png")
+  shades = SimpleCV.ImageClass.Image(d + "glasses.png")
 
-    while True:
-        image = camera.getImage()
+  while True:
+    image = camera.getImage()
 
-        features = image.findHaarFeatures(haar)
+    features = image.findHaarFeatures(haar)
 
-        try:
-            # features.draw(SimpleCV.Color.GREEN)
+    try:
+      # features.draw(SimpleCV.Color.GREEN)
 
-            for i in range(len(features)):
-                feature = features[i]
-                coords = feature.coordinates()
+      for i in range(len(features)):
+        feature = features[i]
+        coords = feature.coordinates()
 
-                image.blit(shades, (coords[0], coords[1]), True)
-        except:
-            pass
+        image.blit(shades, (coords[0], coords[1]), True)
+    except:
+      pass
 
-        image.show()
+    image.show()
 
-        time.sleep(0.05)
+    time.sleep(0.05)
 
 if __name__=="__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
+  try:
+    main()
+  except KeyboardInterrupt:
+    pass
